78fd2dd14dce622c9923de6401e9e4df95bae91b
Udayveer <Udayveernarwaria@gmail.com>
2016-03-26 11:53:14 +0530

    changes on line

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 |---------------------------------------------------------------
 | PHP ERROR REPORTING LEVEL
 |---------------------------------------------------------------
-|
+
 | By default CI runs with error reporting set to ALL.  For security
 | reasons you are encouraged to change this when your site goes live.
 | For more info visit:  http://www.php.net/error_reporting
@@ -41,6 +41,7 @@
 |
 */
 	$application_folder = "application";
+echo "hello world";
 
 /*
 |===============================================================
